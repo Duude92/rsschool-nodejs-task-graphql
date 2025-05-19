bfac1efa49176ff8401f8bd397722c7e2d6d19e7
Duude92 <switch4acc@gmail.com>
2025-05-19 21:59:22 +0300

    Added MemberType

---
+++ src/routes/graphql/types/memberType.ts
@@ -0,0 +1,23 @@
+import {
+  GraphQLEnumType,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql/type/index.js';
+import { GraphQLScalarType } from 'graphql';
+
+
+export const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: { type: GraphQLString },
+    discount: {
+      type: GraphQLFloat,
+    },
+    postsLimitPerMonth: {
+      type: GraphQLInt,
+    },
+  },
+});
