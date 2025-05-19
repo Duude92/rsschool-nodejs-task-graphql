b7c239d5d9c92e1749409cbf5bba42983710d37f
Duude92 <switch4acc@gmail.com>
2025-05-19 22:00:18 +0300

    Usage of MemberType field

--- src/routes/graphql/gql-schema.ts
+++ src/routes/graphql/gql-schema.ts
@@ -1,13 +1,11 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql/type/index.js';
+import { memberTypesField } from './fields/memberTypesField.js';
 
 export const rootSchema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
-      testQuery:{
-        type: GraphQLString,
-        resolve: async ()=> 'Hello World!'
-      }
+      memberTypesField
     }
   }),
 
