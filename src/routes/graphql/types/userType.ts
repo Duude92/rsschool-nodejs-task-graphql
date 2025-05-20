8407b3450091b35c19d2b88ae7532cd29ca04947
Duude92 <switch4acc@gmail.com>
2025-05-20 12:17:18 +0300

    Implementation of User type??? Possible bug of using UserType inside UserType declaration?

---
+++ src/routes/graphql/types/userType.ts
@@ -0,0 +1,41 @@
+import {
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql/type/index.js';
+import { UUIDType } from './uuid.js';
+import { ProfileType } from './profileType.js';
+import { PostType } from './postType.js';
+
+export const UserType = new GraphQLObjectType({
+  name: 'UserType',
+  fields: {
+    id: {
+      type: new GraphQLNonNull(UUIDType),
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    balance: {
+      type: new GraphQLNonNull(GraphQLFloat),
+    },
+
+    profile: {
+      type: ProfileType,
+    },
+
+    posts: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType)))
+    },
+
+    userSubscribedTo: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+    },
+
+    subscribedToUser: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+    },
+  },
+});
