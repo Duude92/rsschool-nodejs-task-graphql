b549bc0155931d7919b02c9ff192c8fac9cd08e4
Duude92 <switch4acc@gmail.com>
2025-05-20 12:14:18 +0300

    Implemented Post type

---
+++ src/routes/graphql/types/postType.ts
@@ -0,0 +1,15 @@
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql/type/index.js';
+import { UUIDType } from './uuid.js';
+
+export const PostType = new GraphQLObjectType({
+  name: 'PostType',
+  fields: {
+    id: { type: new GraphQLNonNull(UUIDType) },
+    title: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    content: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+});
