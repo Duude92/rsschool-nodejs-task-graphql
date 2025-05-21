0fa164afd2aab9bdbbdc89eb182d863bca106f36
Duude92 <switch4acc@gmail.com>
2025-05-21 12:07:21 +0300

    Implementation of query validation for 5-layers depth query request

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { rootSchema } from './gql-schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -17,11 +18,15 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
+      const errors = validate(rootSchema, parse(query), [depthLimit(5)]);
+
+      if (errors.length) return { errors };
       return graphql({
         schema: rootSchema,
         source: query,
         variableValues: variables,
-        contextValue: prisma
+        contextValue: prisma,
+        // validationRules: [depthLimit(5)],
       });
     },
   });
