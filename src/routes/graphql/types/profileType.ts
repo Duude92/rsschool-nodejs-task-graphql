35105c586cdbbf0c60e9552a95ff196a18244b4a
Duude92 <switch4acc@gmail.com>
2025-05-23 16:15:17 +0300

    Fix test-loader with using memberLoader

--- src/routes/graphql/types/profileType.ts
+++ src/routes/graphql/types/profileType.ts
@@ -14,7 +14,11 @@ export const ProfileType = new GraphQLObjectType({
     id: { type: new GraphQLNonNull(UUIDType) },
     isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
     yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
-    memberType: { type: new GraphQLNonNull(MemberType) },
+    memberType: {
+      type: new GraphQLNonNull(MemberType),
+      resolve: async (parent, _, context) =>
+        await context.loaders.memberLoader.load(parent.memberTypeId),
+    },
   },
 });
 export const CreateProfileInput = new GraphQLInputObjectType({
