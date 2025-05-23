f93fc57c6ca6126d0c01f736cda97e1452f24f67
Duude92 <switch4acc@gmail.com>
2025-05-23 18:09:26 +0300

    Added some types // dataloader, context, resolver

---
+++ src/routes/graphql/api/Types.ts
@@ -0,0 +1,20 @@
+import { GraphQLFieldConfig } from 'graphql/type/index.js';
+import { IMemberType, IPost, IProfile, IUserType } from './IObjectTypes.js';
+import { PrismaClient } from '@prisma/client';
+import DataLoader from 'dataloader';
+
+export type UserResolver = GraphQLFieldConfig<IUserType, any, any>;
+export type ResolverBase<T> = GraphQLFieldConfig<T, Context, string[]>;
+export type Context = {
+  prisma: PrismaClient;
+  loaders: {
+    postLoader: PostLoaderType;
+    profileLoader: ProfileLoaderType;
+    memberLoader: MemberLoaderType;
+    userLoader: UserLoaderType;
+  };
+};
+export type PostLoaderType = DataLoader<unknown, Promise<IPost[]>, unknown>;
+export type ProfileLoaderType = DataLoader<unknown, Promise<IProfile[]>, unknown>;
+export type MemberLoaderType = DataLoader<unknown, Promise<IMemberType[]>, unknown>;
+export type UserLoaderType = DataLoader<unknown, Promise<IUserType[]>, unknown>;
