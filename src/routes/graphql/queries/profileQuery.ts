ca0d3642f2328f74c39fca53696c3477711abc0f
Duude92 <switch4acc@gmail.com>
2025-05-23 13:13:56 +0300

    Refactor: using of field include data

--- src/routes/graphql/queries/profileQuery.ts
+++ src/routes/graphql/queries/profileQuery.ts
@@ -2,15 +2,16 @@ import { GraphQLList, GraphQLNonNull } from 'graphql/type/index.js';
 import { ProfileType } from '../types/profileType.js';
 import { UUID } from 'node:crypto';
 import { UUIDType } from '../types/uuid.js';
+import { extractFields } from '../api/extractFields.js';
 
 export const profileQuery = {
   type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(ProfileType))),
-  resolve: async (_, __, context) =>
-    await context.prisma.Profile.findMany({
-      include: {
-        memberType: true,
-      },
-    }),
+  resolve: async (_, __, context, info) => {
+    const fields = extractFields(info, ProfileType);
+    return await context.prisma.Profile.findMany({
+      include: { memberType: !!fields.memberType },
+    });
+  },
 };
 export const profileIdQuery = {
   type: ProfileType,
@@ -19,6 +20,12 @@ export const profileIdQuery = {
       type: new GraphQLNonNull(UUIDType),
     },
   },
-  resolve: async (_, { id }: { id: UUID }, context) =>
-    await context.prisma.Profile.findUnique({ where: { id: id }, include: { memberType: true } }),
+  resolve: async (_, { id }: { id: UUID }, context, info) => {
+    const fields = extractFields(info, ProfileType);
+
+    await context.prisma.Profile.findUnique({
+      where: { id: id },
+      include: { memberType: !!fields.memberType },
+    });
+  },
 };
